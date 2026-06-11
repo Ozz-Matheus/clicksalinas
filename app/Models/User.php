0508b1c8b00504b82f2182e2ecf9e42c95f76bc6
Ozz-Matheus <montesinos.quintana@gmail.com>
2026-06-11 17:17:39 -0600

    feat: implementar interfaz FilamentUser en el modelo User y agregar método canAccessPanel

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, HasRoles, Notifiable;
@@ -41,5 +43,13 @@ class User extends Authenticatable
     public function albums(): HasMany
     {
         return $this->hasMany(Album::class);
+    }
+
+    /**
+     * Determina si el usuario puede acceder al panel de Filament.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
